0075dd7c75bf26dae5d949ca763c6f2e92551a84
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-08-09 13:46:33 +0300

    validate user input

--- lib/view/widgets/custom_button.dart
+++ lib/view/widgets/custom_button.dart
@@ -3,25 +3,29 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../constants.dart';
 
 class CustomButton extends StatelessWidget{
-  const CustomButton({super.key});
+  final void Function()? onTap;
+  const CustomButton({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Center(
-        child: Text(
-            'Add',
-            style: GoogleFonts.poppins(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            )
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+              'Add',
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              )
+          ),
         ),
       ),
     );
